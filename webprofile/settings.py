0a19f2391b91800f5fdcd5b0f64658bcddc51d4e
arunbiju969 <arunbiju969@gmail.com>
2023-01-23 10:23:33 -0500

    modified:   webprofile/settings.py

--- webprofile/settings.py
+++ webprofile/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
+ALLOWED_HOSTS = ['.vercel.app', '.eu.org']
 # ALLOWED_HOSTS = ['localhost']
 
 
