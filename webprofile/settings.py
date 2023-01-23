a5b431e762d81eecd982e45bc2ea62415b96643f
arunbiju969 <arunbiju969@gmail.com>
2023-01-23 09:26:35 -0500

    deleted:    static/images/UC-9a24ef1a-6a16-42c0-9fe7-c3453d73c149.jpg 	deleted:    static/images/cert.png-1.png 	modified:   webprofile/__pycache__/settings.cpython-311.pyc 	modified:   webprofile/__pycache__/urls.cpython-311.pyc 	modified:   webprofile/settings.py 	modified:   webprofile/urls.py

--- webprofile/settings.py
+++ webprofile/settings.py
@@ -157,4 +157,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 #Media 
 MEDIA_URL = 'media/'
-MEDIA_ROOT = BASE_DIR / 'homepage/uploads/media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'homepage','uploads','media')
