2e6d4ab9d9707aa6b3aa5ac724be5c01e08264c8
arunbiju969 <arunbiju969@gmail.com>
2023-01-26 07:34:36 -0500

    renamed:    index.d.ts -> .index.d.ts 	new file:   .vercel.json 	new file:   api/wsgi.py 	modified:   build_files.sh 	new file:   media/about_page/YGMKYNUR_400x400.jpg 	new file:   media/certificate_page/Defence_Dark-arts.png 	new file:   media/certificate_page/Specialization.png 	new file:   media/certificate_page/cert.png 	new file:   media/certificate_page/cert_5UDsnZZ.png 	new file:   media/certificate_page/cert_OYVjrEe.png 	new file:   media/certificate_page/cert_cD1Z8HW.png 	new file:   media/certificate_page/cert_nqpxHCm.png 	renamed:    staticfiles_build/static/images/UC-9a24ef1a-6a16-42c0-9fe7-c3453d73c149.jpg -> media/certificate_page/udemy_django.jpg 	new file:   media/certificate_page/udemy_python.jpg 	new file:   media/contact_page/2021_Twitter_lodfgo_-_white.png 	new file:   media/contact_page/2021_Twitter_logo_-_black.png 	new file:   media/contact_page/2021_Twitter_logo_-_white.png 	new file:   media/contact_page/2021_Twitter_logo_-_white_BDCClq7.png 	new file:   media/contact_page/Twitter_social_icons_-_circle_-_blue.png 	new file:   media/contact_page/Twitter_social_icons_-_circle_-_blue_I1hbcV7.png 	new file:   media/contact_page/Twitter_social_icons_-_circle_-_blue_PagBvNS.png 	new file:   media/contact_page/Twitter_social_icons_-_circle_-_white.png 	new file:   media/contact_page/Twitter_social_icons_-_circle_-_white_lfqERr1.png 	new file:   media/contact_page/github-mark-white.png 	new file:   media/contact_page/github-mark.png 	new file:   media/contact_page/icons8-gmail-logo-128.png 	new file:   media/contact_page/icons8-gmail-logo-128_1iDJmZx.png 	new file:   media/contact_page/icons8-linkedin-100.png 	new file:   media/contact_page/icons8-linkedin-100_fNPzdZz.png 	new file:   media/contact_page/image.png 	new file:   media/contact_page/image_1.png 	new file:   media/contact_page/image_1_OKwvzGK.png 	new file:   media/projects_page/2021_Twitter_logo_-_white.png 	new file:   media/projects_page/ab.png 	new file:   static/CACHE/css/output.38fe0046d36a.css 	new file:   static/CACHE/css/output.5faf27fb254c.css 	new file:   static/CACHE/css/output.858f201beef7.css 	new file:   static/CACHE/css/output.91b2d921392e.css 	new file:   static/CACHE/css/output.95e3bd15b0d8.css 	new file:   static/CACHE/css/output.98f637b32dcb.css 	new file:   static/CACHE/css/output.9a3aecb6a132.css 	new file:   static/CACHE/css/output.9e2e77bc24ba.css 	new file:   static/CACHE/css/output.a450e348a04a.css 	new file:   static/CACHE/css/output.ed5c3a93afe4.css 	new file:   static/CACHE/css/output.f9d47adddba5.css 	deleted:    static/GitHub-Logos/.DS_Store 	modified:   static/src/output.css 	deleted:    staticfiles_build/static/images/cert.png-1.png 	modified:   vercel.json 	modified:   webprofile/settings.py 	modified:   webprofile/urls.py

--- webprofile/settings.py
+++ webprofile/settings.py
@@ -135,7 +135,7 @@ USE_TZ = True
 import os
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+# STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 # Default primary key field type
--- webprofile/urls.py
+++ webprofile/urls.py
@@ -17,12 +17,8 @@ from django.contrib import admin
 from django.urls import include, path
 from webprofile import settings
 from django.conf.urls.static import static
-from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('homepage.urls')),
 ]
-
-urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
